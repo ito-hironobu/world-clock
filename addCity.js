8baac6c9926b8af6a04115fc7374990704922b38
ito.hironobu <ito.hironobu@techcross.co.jp>
2017-05-23 14:33:23 +0900

    restoreByCookie関数を分離した

--- addCity.js
+++ addCity.js
@@ -76,28 +76,3 @@ function createTimePanel(timezone_num){
     $('<div class="clock" id="clock' + timezone_num + '"></div>')
         .appendTo('#panel' + timezone_num);
 }
-
-
-
-// Cookieを使って復元する処理 ////////////////////////
-function restoreByCookie(){
-    for(var key in $.cookie()){
-        // $.cookie('num') はスルー
-        if(key == 'num'){ continue; }
-
-        // checkExistCity関数で、Cookieに登録された都市の行番号抽出
-        var result_city_row = checkExistCity($.cookie(key)) - 1;
-        var timezone_num    = all_data[result_city_row][0];
-        var city            = all_data[result_city_row][1];
-        var country         = all_data[result_city_row][2];
-
-        if( checkExistPanel(result_city_row) ){ // 入力された都市のタイムゾーンパネルにすでにあるなら
-            // パネルの中に追加
-            addCityToPanel(timezone_num, city, country);
-        }else{　// 入力された都市のタイムゾーンパネルにないなら
-            // パネルを作成し、中に都市を追加
-            createTimePanel(timezone_num);
-            addCityToPanel(timezone_num, city, country);
-        }
-    }
-}
